202f2e56d4f3acedae8f27aa67fefb1d8a1d8d03
MasterNeccar <nacaroglua@gmail.com>
2021-11-20 16:16:20 +0300

    Create SQL Eğitimi Ödev 6.sql

---
+++ SQL Eğitimi Ödev 6.sql
@@ -0,0 +1,18 @@
+--Birinci Soru
+SELECT AVG(rental_rate)
+FROM film;
+
+--İkinci Soru 
+SELECT COUNT(*) 
+FROM film
+WHERE title LIKE 'C%';
+
+--Üçüncü Soru
+SELECT MAX(length)
+FROM film
+WHERE rental_rate = 0.99;
+
+--Dördüncü Soru
+SELECT COUNT(DISTINCT replacement_cost)
+FROM film
+WHERE film.length > 150;
